2a62f318d47338589023de0172c1eed679a80fbf
alisa2799 <alisa27guk@gmail.com>
2025-02-27 11:14:06 +0300

    RETURN

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,21 +1,4 @@
-import randomNum from '../bin/randomNum.js';
 import {greet} from '../src/cli.js';
 greet();
-
-
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
-
-const evenNumber = (num) => num % 2 === 0; 
-
-const getQandA = () => {
-    const question = randomNum();
-    const answer = evenNumber(question) ? 'yes' : 'no';
-    return [question, answer];
-    };
-  
-  const initGameEven = () => {
-    greet(rule, getQandA);
-    };
-
-initGameEven();
   
