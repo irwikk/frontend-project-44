b8a6030688b921c0134c1bfddb20af29e4f1efe4
alisa2799 <alisa27guk@gmail.com>
2025-03-04 09:30:33 +0300

    6c

---
+++ src/games/game-calc.js
@@ -0,0 +1,34 @@
+import getRandomNumber from '../../bin/getRandomNumber.js';
+import playGame from '../index.js';
+
+const rule = 'What is the result of the expression?';
+const operations = ['+', '-', '*'];
+
+const getCorrectAnswer = (operand1, operand2, operation) => {
+  let answer;
+  switch (operation) {
+    case '+': answer = operand1 + operand2;
+      break;
+    case '-': answer = operand1 - operand2;
+      break;
+    case '*': answer = operand1 * operand2;
+      break;
+    default: break;
+  }
+  return answer;
+};
+
+const getQuestionAndAnswer = () => {
+  const operand1 = getRandomNumber();
+  const operand2 = getRandomNumber();
+  const operation = operations[getRandomNumber(0, operations.length)];
+  const question = `${operand1} ${operation} ${operand2}`;
+  const answer = getCorrectAnswer(operand1, operand2, operation);
+  return [question, String(answer)];
+};
+
+const initGameCalc = () => {
+  playGame(rule, getQuestionAndAnswer);
+};
+
+export default initGameCalc;
