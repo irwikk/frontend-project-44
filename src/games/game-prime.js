3b2fd56bda5f833d63c26d76ccd3702406ccedb7
alisa2799 <alisa27guk@gmail.com>
2025-03-06 14:00:06 +0300

    9c

---
+++ src/games/game-prime.js
@@ -0,0 +1,24 @@
+import getRandomNumber from '../../bin/getRandomNumber.js';
+import playGame from '../index.js';
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrimeNumber = (num) => {
+  if (num <= 1) return false;
+  for (let i = 2; i < num / 2; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const getQueAndAns = () => {
+  const question = getRandomNumber();
+  const answer = isPrimeNumber(question) ? 'yes' : 'no';
+  return [question, answer];
+};
+
+const initGamePrime = () => {
+  playGame(rule, getQueAndAns);
+};
+
+export default initGamePrime;
