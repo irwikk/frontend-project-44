571086698bffcf49fca14306bfb204a94413c7fc
alisa2799 <alisa27guk@gmail.com>
2025-03-28 13:26:29 +0300

    corrected game-progression

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,23 +1,31 @@
 import getRandomNumber from '../../src/getRandomNumber.js';
 import playGame from '../index.js';
 
-const lengthProgression = 10;
-const maxDiffProgression = 10;
+const progressionLength = 10;
 
 const rule = 'What number is missing in the progression?';
 
-const getQueAndAns = () => {
-  const firstNum = getRandomNumber();
-  const diffProgression = getRandomNumber(1, maxDiffProgression);
-  const skip = getRandomNumber(0, lengthProgression);
+const getArithmeticProgression = () => {
+  let progressionNumber = getRandomNumber(0, 50);
+  const step = getRandomNumber(2, 5);
   const progression = [];
-  for (let i = 0; i < lengthProgression; i += 1) {
-    progression.push(firstNum + diffProgression * i);
+
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression.push(progressionNumber);
+    progressionNumber += step;
   }
-  const answer = progression[skip];
-  progression[skip] = '..';
-  const question = progression.join(' ');
-  return [question, String(answer)];
+  return progression;
+};
+
+const getQueAndAns = () => {
+  const progression = getArithmeticProgression();
+  const randomIndex = getRandomNumber(0, progressionLength);
+
+  const correctAnswer = String(progression[randomIndex]);
+  progression[randomIndex] = '..';
+  const question = progression;
+
+  return [question, correctAnswer];
 };
 
 const initGameProgression = () => {
