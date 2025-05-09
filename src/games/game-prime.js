5b04bd9d9ac391ce2a092a5b364bda750cf2cfad
irwik <shurikgo06@gmaol.com>
2025-05-09 19:12:42 +0300

    prime

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -4,16 +4,20 @@ import playGame from '../index.js';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrimeNumber = (num) => {
-  if (num <= 1) return false;
+  if (num <= 1) {
+    return false;
+  } 
   for (let i = 2; i < num / 2; i += 1) {
-    if (num % i === 0) return false;
+    if (num % i === 0) {
+      return false;
+    }
   }
   return true;
 };
 
 const getQAndA = () => {
   const que = getRandomNumber();
-  const answer = isPrimeNumber(question) ? 'yes' : 'no';
+  const answer = isPrimeNumber(que) ? 'yes' : 'no';
   return [que, answer];
 };
 
